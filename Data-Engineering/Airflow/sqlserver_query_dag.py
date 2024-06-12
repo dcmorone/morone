8d101e201053e9b6e3258e3b2adda74471badaff
dcmorone <103526572+dcmorone@users.noreply.github.com>
2024-06-11 23:31:15 -0300

    Create sqlserver_query_dag.py

---
+++ Data-Engineering/Airflow/sqlserver_query_dag.py
@@ -0,0 +1,35 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.hooks.base_hook import BaseHook
+import pyodbc
+from airflow.utils.dates import days_ago
+
+def execute_query():
+    conn = BaseHook.get_connection('my_sqlserver_connection')
+    conn_str = f"DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={conn.host};DATABASE={conn.schema};UID={conn.login};PWD={conn.password}"
+    
+    connection = pyodbc.connect(conn_str)
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM dbo.DimCustomer")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    connection.close()
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': days_ago(1),
+}
+
+dag = DAG(
+    'sqlserver_query_dag',
+    default_args=default_args,
+    schedule_interval='@daily',
+)
+
+query_task = PythonOperator(
+    task_id='execute_query',
+    python_callable=execute_query,
+    dag=dag,
+)
